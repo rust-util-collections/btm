e54a4eb02e4c99a24bd42b0009c3bec7c4a17550
FanHui <hui@findora.org>
2021-10-11 23:55:09 +0800

    use ruc on crates.io

--- src/api/server/mod.rs
+++ src/api/server/mod.rs
@@ -16,11 +16,7 @@ pub fn run_daemon(cfg: &BtmCfg) -> Result<()> {
     loop {
         if let Ok((msg, peer)) = SERVER_US.recv_128() {
             if let Ok(r) = info!(serde_json::from_slice::<Req>(&msg)) {
-                let success = if info!(cfg.snapshot(r.idx())).is_ok() {
-                    true
-                } else {
-                    false
-                };
+                let success = info!(cfg.snapshot(r.idx())).is_ok();
                 SERVER_US
                     .send(&Resp::new(r.idx(), success).to_bytes(), &peer)
                     .c(d!())?;
